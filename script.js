fb00ffc71bbd92bd76166772a16ffa0083fd81c9
Jon Desserres <jondesserres@gmail.com>
2025-08-06 20:49:54 -0700

    adjusted the hamburger menu

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const hamburger = document.getElementById('hamburger');
+const closeBtn = document.getElementById('closeBtn');
 const navLinks = document.getElementById('nav-links');
+const header = document.querySelector('.header');
 
 hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
     navLinks.classList.toggle('show');
+    header.classList.add('nav-active');
+});
+
+closeBtn.addEventListener('click', () => {
+    navLinks.classList.remove('show');
+    header.classList.remove('nav-active');
 });
